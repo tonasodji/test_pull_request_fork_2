884ed01cadbbbe14355a70db2ea1b420a4316214
sodji98 <107852003+sodji98@users.noreply.github.com>
2022-08-09 08:52:12 +0200

    Create g.c

---
+++ g.c
@@ -0,0 +1,9 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+void g()
+{
+ 
+  int a, b;
+  a = a + a;
+}
