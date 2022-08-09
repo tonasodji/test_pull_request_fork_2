cc1117f652babadb2a831fafcb90494edf2fe619
sodji98 <107852003+sodji98@users.noreply.github.com>
2022-08-09 08:53:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,9 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
-void g()
-{
-  //fix
-  int a, b;
-  a = 0;
-  a = a + a;
-}
-//fix
 int
 main()
 {
   int q, w, e, r, t, y;
-  t=t+t;
+  q=q+w+e+r+t+y;
 }
